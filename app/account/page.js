c8419b3f1ce8a9e576d3e3586cc9ab922ae276df
NidzaX <59400424+NidzaX@users.noreply.github.com>
2025-03-11 10:22:39 +0100

    Adding Nested Routes and Pages

--- app/account/page.js
+++ app/account/page.js
@@ -1,11 +1,11 @@
 export const metadata = {
-  title: "Account",
+  title: "Welcome",
 };
 
 export default function Page() {
   return (
-    <div>
-      <h1>Account page</h1>
-    </div>
+    <h2 className="font-semibold text-2xl text-accent-400 mb-7">
+      Welcome, Nick
+    </h2>
   );
 }
